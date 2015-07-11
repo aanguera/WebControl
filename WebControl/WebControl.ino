6e330f3c125b0c8435b02272f62486f52d49e8df
Toni Anguera <aanguera.65@gmail.com>
2015-07-11 13:20:29 +0200

    Change de server to xibo.inspeguera.cat, read the analog chanel A0 and send the read to the server to be displayed, configure a reconnection when the connetion fail

--- WebControl/WebControl.ino
+++ WebControl/WebControl.ino
@@ -22,15 +22,20 @@
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x0F, 0x73, 0x03 };
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
-IPAddress server(81,169,167,42);  // numeric IP for Google (no DNS)
+IPAddress server(80,32,136,98);  // numeric IP for Google (no DNS)
 //char server[] = "www.google.com";    // name address for Google (using DNS)
 
 boolean lastConnected = false;    // state of the connection last time through the main loop
 // Set the static IP address to use if the DHCP fails to assign
-IPAddress ip(192,168,0,177);
+IPAddress ip(192,168,1,177);
 int led = 7;
 char cLast = '<';  // The character used before
 char c = '<';
+
+// Strings for HTTP request
+String requestString = "GET /cgi-bin/existeix.pl?nivell=";
+String stringRead = "";
+String requestStringFinal = "";
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server 
@@ -70,13 +75,13 @@ void loop()
     Serial.print(c);
   }
   
-  if ( cLast == '\n' && c == '1' )
+  if ( cLast == '>' && c == '1' )
       {
   //      Serial.println("OKOKOKKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKO.");
         digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
         delay(500);
       }
-      if ( cLast == '\n' && c == '0' )
+      if ( cLast == '>' && c == '0' )
       {
   //      Serial.println("OKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKO.");
         digitalWrite(led, LOW);   // turn the LED on (HIGH is the voltage level)
@@ -93,26 +98,32 @@ void loop()
     //while(true);
   }
   if (!client.connected()) {
+    delay(500);
     httpRequest();
   }
   lastConnected = client.connected();
 }
 
 void httpRequest() {
+  stringRead =  String(analogRead(A0), DEC);
+  requestStringFinal = String(requestString + stringRead);
   Serial.println("connecting...");
 
   // if you get a connection, report back via serial:
   if (client.connect(server, 80)) {
     Serial.println("connected");
     // Make a HTTP request:
-    client.println("GET /test.html HTTP/1.1");
-    client.println("Host: inskta.cat");
+    client.println(requestStringFinal);
+    client.println("Host: xibo.inspeguera.cat");
     client.println("Connection: close");
     client.println();
   } 
   else {
     // kf you didn't get a connection to the server:
     Serial.println("connection failed");
+    // If conection fail, delay and stop the connection to be ready for a new one
+    delay(5000);
+    client.stop();
   }
 }
 
