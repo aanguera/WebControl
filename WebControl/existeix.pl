58f1008f1d47d87e5d55197f764ab350bc6516ba
Toni Anguera <aanguera.65@gmail.com>
2015-08-03 21:35:45 +0200

    The update of the database was missing, new 3 lines are added

--- WebControl/existeix.pl
+++ WebControl/existeix.pl
@@ -2,6 +2,9 @@
 use CGI;                             # load CGI routines
 $q = CGI->new;                       # create new CGI object
 print $q->header;                    # create the HTTP header
+$nivell = $q->param('nivell');
+$nivell += 0;
+`/usr/bin/rrdupdate /var/local/nivell.rrd N:$nivell`;
 if (-e "/tmp/existeixo.flag") {
         print '<text>1</text>';      # level 1 header
 } else {
